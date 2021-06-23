2e70f99c41c2a38c73edee2b1976205deaa7c42b
Gabriel Magreti <gabrielmagreti@gmail.com>
2021-06-22 23:50:54 -0300

    Finalizando app

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,6 +5,7 @@ import { Grid } from './styles';
 import ServerList from '../ServerList';
 import ServerName from '../ServerName';
 import ChannelInfo from '../ChannelInfo';
+import ChannelData from '../ChannelData';
 import ChannelList from '../ChannelList';
 import UserInfo from '../UserInfo';
 import UserList from '../UserList';
@@ -16,7 +17,7 @@ const Layout: React.FC = () => {
       <ServerName />
       <ChannelInfo />
       <ChannelList />
-
+      <ChannelData />
       <UserInfo />
       <UserList />
     </Grid>
